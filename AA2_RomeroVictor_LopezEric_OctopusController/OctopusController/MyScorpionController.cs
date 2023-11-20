cc65e8fb5ea4f344836473d5f751e8db830f088f
Eriicloo <eric.lopez@enti.cat>
2023-11-20 10:54:08 +0100

    Started Gradient Descent and Forward Kinematics

--- AA2_RomeroVictor_LopezEric_OctopusController/OctopusController/MyScorpionController.cs
+++ AA2_RomeroVictor_LopezEric_OctopusController/OctopusController/MyScorpionController.cs
@@ -15,19 +15,30 @@ namespace OctopusController
         Transform tailEndEffector;
         MyTentacleController _tail;
         float animationRange;
+        //TAIL
+
+        Vector3[] axis;
+        Vector3[] startingOffsets;
+
+        float stopThreshold;
+        float learningRate;
+        float[] solutionsTail;
+
 
         //LEGS
         Transform[] legTargets;
         Transform[] legFutureBases;
         MyTentacleController[] _legs = new MyTentacleController[6];
+        //LEGS
 
-        
+
         #region public
         public void InitLegs(Transform[] LegRoots,Transform[] LegFutureBases, Transform[] LegTargets)
         {
             _legs = new MyTentacleController[LegRoots.Length];
+
             //Legs init
-            for(int i = 0; i < LegRoots.Length; i++)
+            for (int i = 0; i < LegRoots.Length; i++)
             {
                 _legs[i] = new MyTentacleController();
                 _legs[i].LoadTentacleJoints(LegRoots[i], TentacleMode.LEG);
@@ -40,13 +51,40 @@ namespace OctopusController
         {
             _tail = new MyTentacleController();
             _tail.LoadTentacleJoints(TailBase, TentacleMode.TAIL);
+
             //TODO: Initialize anything needed for the Gradient Descent implementation
+            axis = new Vector3[_tail.Bones.Length];
+            stopThreshold = 0.5f;
+            learningRate = 0.2f;
+            startingOffsets = new Vector3[_tail.Bones.Length];
+            solutionsTail = new float[_tail.Bones.Length];
+
+            for (int i = 0;i < _tail.Bones.Length;i++) 
+            {
+                if (i == 0)
+                {
+                    axis[i] = Vector3.up;
+                    solutionsTail[i] = _tail.Bones[i].localRotation.eulerAngles.y;
+                }
+                else 
+                {
+                    axis[i] = Vector3.right;
+                    solutionsTail[i] = _tail.Bones[i].localRotation.eulerAngles.x;
+                }
+            }
+
+            for (int i = 0; i < _tail.Bones.Length - 1; i++)
+            {
+                startingOffsets[i] = Quaternion.Inverse(_tail.Bones[i].rotation) * (_tail.Bones[i + 1].position - _tail.Bones[i].position);
+            }
+
+
         }
 
         //TODO: Check when to start the animation towards target and implement Gradient Descent method to move the joints.
         public void NotifyTailTarget(Transform target)
         {
-
+            tailTarget = target;
         }
 
         //TODO: Notifies the start of the walking animation
@@ -81,6 +119,29 @@ namespace OctopusController
         {
 
         }
+
+        private Vector3 ForwardKinematics(float[] solutions) 
+        {
+            Vector3 lastPosition = _tail.Bones[0].position;
+            Quaternion rotation = _tail.Bones[0].parent.rotation;
+
+            for (int i = 1; i < _tail.Bones.Length; i++)
+            {
+                rotation *= Quaternion.AngleAxis(solutions[i - 1], axis[i - 1]);
+                Vector3 nextPosition = lastPosition + rotation * startingOffsets[i - 1];
+                lastPosition = nextPosition;
+            }
+
+            return lastPosition;
+        }
+
+        private float DistanceFromTarget(Vector3 target, float[] solutions) 
+        {
+            Vector3 point = ForwardKinematics(solutions);
+            return Vector3.Distance(point, target);
+        }
+
+
         #endregion
     }
 }
