b93b7de47aea0657a09012057e4373c792909099
Eric Lopez <eric.lopez@enti.cat>
2024-01-02 19:50:29 +0100

    Unity Project, still doing the minifootball & some scorpion & tentacles

---
+++ AA2_RomeroVictor_LopezEric_OctopusController/MyUnityProject/Assets/Scripts/Other/Reset.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+    [SerializeField] private Transform ball;
+    [SerializeField] private Transform scorpion;
+
+    private Vector3 ballPos;
+    private Vector3 scorpionPos;
+
+    private void Awake()
+    {
+        ballPos = ball.position;
+        scorpionPos = scorpion.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ball.position = ballPos;
+            scorpion.position = scorpionPos;
+        }
+    }
+}
